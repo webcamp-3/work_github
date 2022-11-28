11eb5dec8feb83a46b1cf5134a30cbf69dccee76
wataru2461 <112701392+wataru2461@users.noreply.github.com>
2022-11-28 10:13:52 +0000

    Initial Commit5

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,17 +6,25 @@ class Public::AddressesController < ApplicationController
 
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     @address.save
-    redirect_to public_address(@address.id)
+    redirect_to public_addresses_path
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to public_addresses_path
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to public_addresses_path
   end
   
   def address_params
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,10 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @genres = Genre.all
   end
 
   def show
+    
   end
 end
