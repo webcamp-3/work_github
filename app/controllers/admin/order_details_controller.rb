aaaddb7bc08a11f40f93efe11d5aecfd9e1113aa
wataru2461 <112701392+wataru2461@users.noreply.github.com>
2022-12-11 12:37:13 +0000

    Initial Commit8

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,4 +1,13 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    @order = @order_detail.order
+    redirect_to admin_order_path(@order.id)
+  end
+
+  private
+  def order_detail_params
+    params.require(:order_detail).permit(:item_id, :order_id, :purchase_price, :quantity, :production_status )
   end
 end
