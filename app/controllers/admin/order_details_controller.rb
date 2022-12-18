b09934d6e570d8e6312ffefd9a0948bfc9dc57c9
wataru2461 <112701392+wataru2461@users.noreply.github.com>
2022-12-18 13:24:24 +0000

    Initial Commit13

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,8 +1,18 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
-    @order_detail.update(order_detail_params)
     @order = @order_detail.order
+    @order_details = @order.order_details.all
+    is_updated = true
+    if @order_detail.update(order_detail_params)
+      @order.update(status: 2) if @order_detail.production_status == "in_production"
+      @order_details.each do |order_detail|
+        if order_detail.production_status != "completion_of_production"
+          is_updated = false
+        end
+      end
+      @order.update(status: 3) if is_updated
+    end
     redirect_to admin_order_path(@order.id)
   end
 
